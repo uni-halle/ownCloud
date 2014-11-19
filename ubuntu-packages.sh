6710c3b764ee2a98fea04f8987c01a195049bc2d
Robert Jäckel <robert.jaeckel@itz.uni-halle.de>
2014-11-19 14:15:09 +0100

    neue Datei zum installieren der cloud-pakete unter ubuntu 14.04 LTS

---
+++ ubuntu-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+packages=$(cat <<EOF
+  libapache2-mod-php5
+  libav-tools
+  libreoffice-writer
+  php-apc
+  php-mime-type
+  php5-ldap
+  smbclient
+  php5-mysql
+  php5-json
+  php5-imagick
+  php5-gd
+  postfix 
+EOF
+)
+if [ "$#" -lt 1 ]; then
+	bin=$(basename $0)
+	if [ $EUID -ne 0 ]; then bin="sudo $bin"; fi
+	echo "usage:"
+	echo "  $bin install"
+	echo
+	echo "this would install following packages:"
+	echo "$packages"
+fi
+if [[ "$1" = "install" ]]; then
+	$(which apt-get) install $packages
+fi
